e39af4f2c724bf4eae58e1e6f05c0ea8aae9ee02
Diivvuu <divyanshubawa9@gmail.com>
2024-01-24 10:16:13 +0530

    added cursor and cursor blur

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
 document.addEventListener("mousemove", function(dets){
     crsr.style.left = dets.x + "px"
     crsr.style.top = dets.y + "px"
+    blur.style.left = dets.x + "px"
+    blur.style.top = dets.y + "px"
 })
