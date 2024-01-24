aaee07f89714f40b50d5ddb2e9bf94520e999b5e
Diivvuu <divyanshubawa9@gmail.com>
2024-01-24 10:20:08 +0530

    cursor blur adjusted

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ var blur = document.querySelector("#cursor-blur")
 document.addEventListener("mousemove", function(dets){
     crsr.style.left = dets.x + "px"
     crsr.style.top = dets.y + "px"
-    blur.style.left = dets.x + "px"
-    blur.style.top = dets.y + "px"
+    blur.style.left = dets.x - 250 + "px"
+    blur.style.top = dets.y -250 + "px"
 })
