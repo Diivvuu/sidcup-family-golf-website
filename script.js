c0e1e8f9f7d7862898c3ab745ce47db8b399ad90
Diivvuu <divyanshubawa9@gmail.com>
2024-01-26 18:04:57 +0530

    applying javascript on navbar

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ document.addEventListener("mousemove", function(dets){
     crsr.style.top = dets.y + "px"
     blur.style.left = dets.x - 250 + "px"
     blur.style.top = dets.y -250 + "px"
+});
+
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+        crsr.style.scale = 3;
+        crsr.style.border = "1px solid #fff";
+        crsr.style.backgroundColor = "transparent";
+    });
+    elem.addEventListener("mouseleave", function(){
+        crsr.style.scale = 1;
+        crsr.style.border = "0px solid #95c11e";
+        crsr.style.backgroundColor = "#95c11e";
+    })
 })
 
-// cursor on navbar pending
-
-//navbar gsap pending
-// gsap.to("#nav", {
-    // backgroundColor: #000,
-    // 
-// })
